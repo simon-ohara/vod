57bd2796af860de2565d63a55ff0774daa39b5b1
Simon O'Hara <simon.ohara@sage.com>
2012-01-27 16:30:00 +0000

    Compressed Sass Output

--- _plugins/sass_converter.rb
+++ _plugins/sass_converter.rb
@@ -1,8 +1,11 @@
+
+
 module Jekyll
   # Sass plugin to convert .scss to .css
   # 
   # Note: This is configured to use the new css like syntax available in sass.
   require 'sass'
+
   class SassConverter < Converter
     safe true
     priority :low
@@ -18,7 +21,7 @@ module Jekyll
     def convert(content)
       begin
         puts "Performing Sass Conversion."
-        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./assets/styles"])
+        engine = Sass::Engine.new( content, :syntax => :scss, :load_paths => ["./assets/styles"], :style => :compressed )
         engine.render
       rescue StandardError => e
         puts "!!! SASS Error: " + e.message
